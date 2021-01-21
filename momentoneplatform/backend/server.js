9cfe34c687c9825055070e3bbdb9f9cc71f74be0
Sabin Constantin Lungu <sabinlungu@Sabins-Mac-mini.broadband>
2021-01-21 13:05:30 +0000

    Finished adding basic setup

--- momentoneplatform/backend/server.js
+++ momentoneplatform/backend/server.js
@@ -1,2 +1,4 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
--- momentoneplatform/src/App.js
+++ momentoneplatform/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className = "main-header">
-      <h1>Moment one Platform</h1>
+    <div className = "main__container">
+      <h1>Moment One</h1>
     </div>
   );
 }

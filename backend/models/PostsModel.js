ecf461ab2a66346b91c00d928c54447bc1bb8b24
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-08 16:20:51 +0000

    Added communication front end and back end

--- backend/models/PostsModel.js
+++ backend/models/PostsModel.js
@@ -1,6 +1,22 @@
 const mongoose = require('mongoose');
 const PostSchema = new mongoose.Schema({
-    
+    id: {
+        type: Number,
+        unique: true,
+        required: true
+    },
+
+    title: {
+        type: String,
+        unique: false,
+        required: true
+    },
+
+    description: {
+        type: String,
+        unique: false,
+        required: true
+    }
 });
 
 mongoose.model('PostSchema', PostSchema);

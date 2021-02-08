79739973006b9120f76262236d4dc54981ae5834
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-08 14:04:58 +0000

    Added MongoDB connection:

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const port = 8080;
 const okCode = 200;
 const notFound = 404;
 
+const mongoose = require('mongoose');
+const connectionURI = 'mongodb+srv://admin:N@p1er123@momentone-cl.tbcfu.mongodb.net/MomentOne?retryWrites=true&w=majority';
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(morgan('dev'));
 app.use(express.static('public'));
@@ -15,6 +17,22 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded({extended: true}));
 app.use(bodyParser.urlencoded({extended: false}));
+
+mongoose.connect(connectionURI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB instance');
+});
+
+mongoose.connection.on('error', (error) => {
+    console.error('Error connecting to MongoDB -> reason : ', error);
+})
+
 
 app.get('/', (request, response) => {
     try {

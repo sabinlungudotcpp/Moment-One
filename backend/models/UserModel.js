a57b727a394bbecab50c13545c6b3799d69bfaf8
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-09 16:46:27 +0000

    Refactoring back end APIs for handling posts

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,9 +1,10 @@
 //User model schema for MomentOne platform user accounts
 const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
+	
 	username: {
 		type: String, 
-		unique: [true, 'Username taken']
+		unique: [true, 'Username taken'],
 		required: [true, 'Username required'],
 		match: [/^[a-zA-Z0-9]+$/, 'Invalid username'], //Regex. Allows Alphanumeric usernames with no special characters.
 		index: true
@@ -49,16 +50,19 @@ const UserSchema = new mongoose.Schema({
 			type: String,
 			required: true
 		},
+
 		reason: {
 			type: String,
 			required: true
 		},
+
 		length: {
 			type: Number,
 			min: 2,
 			max: 60,
 			required: true
 		},
+
 		reward: {
 			type: String,
 			required: true
@@ -66,11 +70,8 @@ const UserSchema = new mongoose.Schema({
 	},
 
 	therapist: ObjectId, //ObjectId linking to assigned therapists
-
 	posts: [ObjectId], //Arrey of post ids
-
 	profileImage: String, //Path to profile picture file 
-
 	banner: String //Path to banner picture file
 });
 

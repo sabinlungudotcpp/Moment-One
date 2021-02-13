df6cd5efe55be31e49e6878c846e6feaa2c99191
JKos-Git <jakubkosarzecki@hotmail.com>
2021-02-13 15:30:41 +0000

    Changes to userController. fixing syntax and finalizing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,9 @@ exports.getAllUsers = async (request, response) => { //get all users
 		const method = request.method;
 		if(method === 'GET') {
 			const allUsers  = await Users.find(); //Getting users from the database
-			return response.json(allUsers);
+			return response.status(200).json({ //http code 200 (OK)
+				allUsers
+			});
 		}
 	}
 	catch(error) { //Catching errors
@@ -24,7 +26,9 @@ exports.getUserById = async (request, response) => { //Get a user by _id
 		if(method === 'GET') {
 			const id = request.params.id; //Request parameters
 			const userId = await User.findById(id); 
-			return response.status(200).json({userId}); //http code 200 (OK)
+			return response.status(200).json({ //http code 200 (OK)
+			userId
+			}); 
 		}
 	}
 	catch(error) {
@@ -42,12 +46,14 @@ exports.getUserByUserName = async (request, request) => { //Get user by username
 		if(method === 'GET') {
 			const body = request.body; //Getting the username
 			const userName = await User.findOne().where('username').equals(body); //searches for users using the provided username 
-			return respose.status(200).json(userName); //http code 200 (OK)
+			return respose.status(200).json({ //http code 200 (OK)
+			userName
+			}); 
 		}
 	}
 	catch(error) {
 		if(error) {
-			return response.json({ 
+			return response.status(500).json({ 
 				message: error.message
 			});
 		}
@@ -78,6 +84,74 @@ exports.createNewUser = async (request, response) => { //Create a new user
 	catch(error){
 		if(error) {
 			return console.error(error);
+		}
+	}
+};
+
+exports.editUser = async (request, response) => {
+	try {
+		const method = request.method;
+		const id = request.parmas.id;
+
+		if(!isNaN(id)) { //Tests if id is a number. not(not-a-number)
+			return response.status(500).json({ //http error code 500 (Internal server error). Generic
+				message: 'ID invalid'
+			});
+		}
+
+		if(method === 'PATCH') {
+			const updateUser = await User.findByIdAndUpdate(id, request.body); //Finds user by id and then updates with content of request body.
+			return response.status(200).json({ //http code 200 (OK)
+				updateUser, updatedAt: date.now()
+			});
+		}
+	}
+	catch(error) {
+		if(error) {
+			return response.status(500).json({ 
+				message: error.message
+			});
+		}
+	}
+};
+
+exports.deleteAllUsers = async (request, response) => {
+	try {
+		const method = request.method;
+		if(method === 'DELETE') {
+			await User.deleteMany(); // Delete all users from database
+			return  response.status(200).json({ //http code 200 (OK)
+				message: 'All users successfully deleted',
+				deletedAt: date.now()
+			});
+		}
+	}
+	catch(error) {
+		if(error) {
+			return response.status(500).json({ 
+				message: error.message
+			});
+		}
+	}
+};
+
+exports.deleteUserById = async (request, response) => { //Delete a user by id
+	try {
+		const method = request.method;
+		const id = request.params.id;
+		if(method === 'DELETED') {
+			await User.findByIdAndDelete(id);
+			return response.status(200).json({ //http code 200 (OK)
+				message: 'User deleted successfully',
+				deletedAt: date.now()
+			});
+		}
+	}
+	catch(error) {
+		if(error) {
+			return response.status(500).json({ 
+				message: error.message
+			});
 		}
 	}
 };

2e3c4cf14b6aca497eb0791927ad9db601b1ce28
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-15 15:28:32 +0000

    Adding goal controller code

--- backend/controllers/goalsController.js
+++ backend/controllers/goalsController.js
@@ -3,22 +3,54 @@ const Goals = mongoose.model('Goals');
 
 exports.getAllGoals = async (request, response) => {
     try {
-        
+        const method = request.method;
+
+        if(method === 'GET') {
+            const goals = await Goals.find();
+        }
+    } 
+    
+    catch(error) {
+
     }
 };
 
 exports.getGoalByID = async (request, response) => {
+    try {
 
+    } 
+    
+    catch(error) {
+
+    }
 };
 
 exports.editGoal = async (request, response) => {
+    try {
 
+    } 
+    
+    catch(error) {
+
+    }
 }
 
 exports.deleteGoals = async (request, response) => {
+    try {
 
+    } 
+    
+    catch(error) {
+
+    }
 }
 
 exports.deleteGoalByID = async (request, response) => {
+    try {
 
-}
+    } 
+    
+    catch(error) {
+
+    }
+}  

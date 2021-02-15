ffd57274ddcb5a6b42355ba6cd9eaea48c8e3115
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-15 17:44:03 +0000

    Added code to create a new goal

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const goalsController = require('../controllers/goalsController');
 const goalRouter = express.Router();
 
-goalRouter.route('/').get(goalsController.getAllGoals).post(goalsController.createGoal);
-goalRouter.route('/:id').get(goalsController.getGoalByID).patch(goalsController.editGoal);
+goalRouter.route('/').get(goalsController.getAllGoals).post(goalsController.createGoal).delete(goalsController.deleteGoals);
+goalRouter.route('/:id').get(goalsController.getGoalByID).patch(goalsController.editGoal).delete(goalsController.deleteGoalByID);
 
 module.exports = goalRouter;

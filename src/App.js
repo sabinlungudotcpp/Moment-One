815af2cf1215bae289aa7618cbbc8b93581444ce
RSpowart <36923570+RSpowart@users.noreply.github.com>
2021-02-16 18:24:39 +0000

    Update to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
+
 import React from "react";
 import "./css/style.css";
-import NavBar from './components/NavBar';
-import CommunityMembers from './components/CommunityMembers';
-import CommunityMoments from './components/CommunityMoments';
-import Home from './components/Home';
-import SelfAwareness from './components/SelfAwareness';
-import Settings from './components/Settings';
-import UserProfile from './components/UserProfile';
-import YourMoments from './components/YourMoments';
-import {BrowserRouter as Router, Switch, Route} from './components/node_modules/react-router-dom';
+
+import NavBar from './Componenets/NavBar';
+import CommunityMembers from './Componenets/CommunityMembers';
+import CommunityMoments from './Componenets/CommunityMoments';
+import Home from './Componenets/Home';
+import SelfAwareness from './Componenets/SelfAwareness';
+import Settings from './Componenets/Settings';
+import UserProfile from './Componenets/UserProfile';
+import YourMoments from './Componenets/YourMoments';
+
+import {BrowserRouter as Router, Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className = "App">
-      <header className = "App-header">
-        <h1> Moment One Home</h1>
-      </header>
-    </div>
+    <Router>
+      <div className = "App">
+        
+        <NavBar />
+        
+        <div className = "PageContent">
+          <Switch>
+            <Route path="/" exact component = {Home}/>
+            <Route path="/CommunityMembers" component={CommunityMembers}/>
+            <Route path="/CommunityMoments" component={CommunityMoments}/>
+            <Route path="/Home" component={Home}/>
+            <Route path="/SelfAwareness" component={SelfAwareness}/>
+            <Route path="/YourProfile" component={UserProfile}/>
+            <Route path="/YourMoments" component={YourMoments}/>
+            <Route path="/Settings" component={Settings}/>
+          </Switch>
+          </div>
+      </div>
+    </Router>
   )
 }
 
-export default App; // export the app
+export default App;

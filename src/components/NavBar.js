099f96badece8543c0e8d07b59ae4a94b934e383
RSpowart <36923570+RSpowart@users.noreply.github.com>
2021-02-22 12:14:07 +0000

    Completed navigation bar
    fixed some minor incorrect nav-bar functionality. Removed outdated, unneeded or incorrect images.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,5 @@
 import React, { Component } from "react";
 import { NavLink } from 'react-router-dom';
-//icon imports
-import homeIcon from '../imageAssets/MenuBarIcons/home.png';
-import chat from '../imageAssets/MenuBarIcons/chat.png';
-import community from '../imageAssets/MenuBarIcons/community.png';
-import perspectives from '../imageAssets/MenuBarIcons/perspectives.png';
-import wellbeing from '../imageAssets/MenuBarIcons/wellbeing.png';
-import logout from '../imageAssets/MenuBarIcons/logout.png';
 
 const Navbar = () => { // Navigation Bar Component
     return (
@@ -24,7 +17,7 @@ const Navbar = () => { // Navigation Bar Component
                 <li className="moments">
                     <NavLink activeClassName="navbar_link_active" className="navbar_link" to = "/home">
                         <div className = "icon">
-                            <img src= {homeIcon}></img>
+                            <div className ="homeIcon"></div>
                         </div>
                         <span className="navbar_items-text">Dashboard</span>
                     </NavLink>
@@ -36,7 +29,7 @@ const Navbar = () => { // Navigation Bar Component
                 <li>
                     <NavLink exact activeClassName="navbar_link_active" className="navbar_link" to="/wellbeing">
                         <div className = "icon">
-                            <img src= {wellbeing}></img>
+                            <div className ="wellbeingIcon"></div>
                         </div>
                         <span className="navbar_items-text">Wellbeing</span>
                     </NavLink>
@@ -44,7 +37,7 @@ const Navbar = () => { // Navigation Bar Component
                 <li>
                     <NavLink exact activeClassName="navbar_link_active" className="navbar_link" to = "/perspectives">
                         <div className = "icon">
-                            <img src= {perspectives}></img>
+                            <div className ="perspectivesIcon"></div>
                         </div>
                         <span className="navbar_items-text">Perspectives</span>
                     </NavLink>
@@ -52,7 +45,7 @@ const Navbar = () => { // Navigation Bar Component
                 <li>
                     <NavLink exact activeClassName="navbar_link_active" className="navbar_link" to="/community">
                         <div className = "icon">
-                            <img src= {community}></img>
+                            <div className ="communityIcon"></div>
                         </div>
                         <span className="navbar_items-text">Community</span>
                     </NavLink>
@@ -60,7 +53,7 @@ const Navbar = () => { // Navigation Bar Component
                 <li>
                     <NavLink exact activeClassName="navbar_link_active" className="navbar_link" to="livechat">
                         <div className = "icon">
-                            <img src= {chat}></img>
+                            <div className ="livechatIcon"></div>
                         </div>
                         <span className="navbar_items-text">Live-Chat</span>   
                     </NavLink>
@@ -68,7 +61,7 @@ const Navbar = () => { // Navigation Bar Component
                 <li className="logout">
                     <NavLink exact activeClassName="navbar_link_active" className="navbar_link" to="/logout">
                         <div className = "icon">
-                            <img src= {logout}></img>
+                            <div className ="logoutIcon"></div>
                         </div>
                         <span className="navbar_items-text">Log-Out</span>
 

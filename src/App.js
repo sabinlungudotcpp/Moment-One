6f8669b2a272bfe19243e92f610d038cd63de37c
Sabin Constantin Lungu <sabinlungu@Sabins-Mac-mini.broadband>
2021-03-02 10:39:30 +0000

    Refactoring app.js

--- src/App.js
+++ src/App.js
@@ -2,46 +2,25 @@ import React from "react";
 import "./css/style.css";
 
 import NavBar from './components/NavBar';
-import livechat from './components/LiveChat';
-import community from './components/Community';
-import home from './components/Home';
-import perspectives from './components/Perspectives';
-import settings from './components/Settings';
-import wellbeing from './components/Wellbeing';
-import yourjourney from './components/YourJourney';
+import Home from './components/Home';
+import Settings from './components/Settings';
+import SelfAwareness from './components/SelfAwareness';
+
 
 import {BrowserRouter as Router, Switch,Route} from 'react-router-dom';
 
 const App = () => { // Main App Component
   return (
-<<<<<<< HEAD
-
-=======
->>>>>>> 35fb59267f5a47fc739995260de62cdd0f07c5d5
     <Router>
       <div className = "App">
         <NavBar />
 
         <div className = "PageContent">
           <Switch>
-<<<<<<< HEAD
             <Route path="/" exact component = {Home}/>
-            <Route path="/CommunityMembers" component = {CommunityMembers}/>
-            <Route path="/CommunityMoments" component = {CommunityMoments}/>
             <Route path="/Home" component = {Home}/>
             <Route path="/SelfAwareness" component={SelfAwareness}/>
-            <Route path="/YourProfile" component = {UserProfile}/>
-            <Route path="/YourMoments" component={YourMoments}/>
             <Route path="/Settings" component={Settings}/>
-=======
-            <Route path = "/home" exact component = {home}/>
-            <Route path = "/home/yourjourney" exact component = {yourjourney}/>
-            <Route path = "/wellbeing" component = {wellbeing}/>
-            <Route path = "/perspectives" component = {perspectives}/>
-            <Route path = "/community" component = {community}/>
-            <Route path = "/livechat" component = {livechat}/>
-            <Route path = "/settings" component = {settings}/>
->>>>>>> 35fb59267f5a47fc739995260de62cdd0f07c5d5
           </Switch>
 
           </div>

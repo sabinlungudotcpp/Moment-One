32f0ffeccb81b82f733e685de2b5c957f1e54424
Sabin Constantin Lungu <sabinlungu@Sabins-Mac-mini.broadband>
2021-03-03 14:40:53 +0000

    Added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,24 @@
 import React from "react";
 import searchIcon from "../imageAssets/Icons/General/search.png"
+
 const Header = (props) => {
   return (
-    <div className = "header">
+
+  <div className = "header">
     <h1> {props.title}</h1>
       <div className = "searchBell">
+
         <form className="searchbar">
           <img className = "seachIcon" src={searchIcon} />
           <input type="text" id = "search" className="search"></input>
         </form>
+
+
         <div className = "bellIcon"></div>
       </div> 
     </div>
+
+
   )
 }
 
--- src/components/Journal.js
+++ src/components/Journal.js
@@ -1,17 +1,19 @@
 import React from "react";
-import axios from'axios';
-
+import axios from 'axios';
 import JournalMoment from "./journal_moment";
+
 class momentForm extends React.Component{
-  //tracking the user selected category
-    state ={
+
+    state = {
         Category:'',
     }
+
     change = (e) => {
         this.setState({
             [e.target.name]: e.target.value
         })
     }
+
     render(){
         return (
             <div className="journal">
@@ -23,6 +25,7 @@ class momentForm extends React.Component{
                 <button class="jounral_categories">Categories</button>
               </form>
             <div className="momentList">
+
             {/*Temporary filler until axios is implemented */}
             <JournalMoment title="Lockdown woes is the title" content="Oh boy is lockdown boring, this would be the content" date="March 21,2021" />
             <JournalMoment title="Lockdown woes is the title" content="Oh boy is lockdown boring, this would be the content" date="March 21,2021" />
@@ -30,10 +33,7 @@ class momentForm extends React.Component{
             <JournalMoment title="Lockdown woes is the title" content="Oh boy is lockdown boring, this would be the content" date="March 21,2021" />
             <JournalMoment title="Lockdown woes is the title" content="Oh boy is lockdown boring, this would be the content" date="March 21,2021" />
             </div>
-
-
-
-            </div>
+          </div>
           )
     }
 }

be4a3dab992d65a1f38a51652be89fc44fcae325
Sabin Constantin Lungu <sabinlungu@Sabins-Mac-mini.broadband>
2021-03-04 21:20:06 +0000

    Added a course model

--- backend/models/courseModel.js
+++ backend/models/courseModel.js
@@ -1,6 +1,34 @@
 const mongoose = require('mongoose');
-const courseSchema = new mongoose.Schema({
 
+const courseSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'A course must have a title']
+    },
+
+    description: {
+        type: String,
+        required: [true, 'A post must have a description']
+    },
+
+    image: {
+        type: Image,
+        required: [false, 'An image is not required for now']
+    },
+
+    isCompleted: {
+        type: Boolean,
+        default: false
+    },
+
+    inProgress: {
+        type: Boolean,
+        default: false
+    },
+
+    coursesCompleted: {
+        type: Number
+    }
 });
 
 const Course = mongoose.model('Course', courseSchema);

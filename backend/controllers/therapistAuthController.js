6f5a1152b9c3e1feff15a5b8c82a9745167975d2
Sabin Constantin Lungu <sabinlungu@Sabins-Mac-mini.broadband>
2021-03-04 18:01:34 +0000

    Added code to register a therapist

--- backend/controllers/therapistAuthController.js
+++ backend/controllers/therapistAuthController.js
@@ -6,6 +6,7 @@ const okCode = 200;
 const created = 201;
 const unauthorized = 401;
 const unprocessable = 422;
+const serverError = 500;
 
 const signToken = (id) => {
     return jwt.sign({id}, 'MY_SECRET_KEY');
@@ -43,7 +44,36 @@ exports.registerTherapist = async (request, response, next) => { // Middleware f
 
 exports.loginTherapists = async (request, response, next) => {
     try {
+        // Logs in the therapist
+        const { username, password } = request.body;
+        const method = request.method;
 
+        if(!username || !password) {
+            return response.status(serverError).json({
+                status: 'Login Failed',
+                message: 'You must provide a username and password'
+            });
+        }
+
+        if(method === 'POST') {
+            const therapist = await Therapist.findOne({username});
+
+            if(!therapist || !(await therapist.comparePasswords(password, therapist.password))) { // If there is no password or the passwords don't match.
+
+                return response.status(unauthorized).json({
+                    message: 'Incorrect e-mail or password',
+                    sentAt: new Date().toISOString()
+                });
+            }
+
+            const token = signToken(therapist._id);
+            return response.status(okCode).json({
+
+                status: 'Logged In Success',
+                message: `You are logged in as ${username} with the token ${token}`,
+                loggedInAt: new Date().toISOString()
+            });
+        }
     } 
     
     catch(error) {

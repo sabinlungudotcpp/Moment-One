bb628adbae324b75577d8f29e820a967f3e43f54
Sabin Constantin Lungu <sabinlungu@Sabins-Mac-mini.broadband>
2021-03-04 15:34:44 +0000

    Adding code to the TherapistModel

--- backend/models/therapistModel.js
+++ backend/models/therapistModel.js
@@ -1,27 +1,34 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
+const HASH_BYTES = 12;
 
 const TherapistSchema = new mongoose.Schema({ //Therapist schema that inherits from the base schema
 
 	firstName: {
 		type: String,
-		required: [true, 'Firat name required']
-		},
+		required: [true, 'First name required']
+	},
 
 	lastName: {
 		type: String,
 		required: [true, 'Last name required']
 	},
 
-	username: {
+	username: { // The username of the therapist model
 		type: String,
 		min: 3,
 		required: [true, 'As a therapist you must specify your username']
 	},
 
 	password: {
+		type: String,
+		required: [true, 'As a therapist you must provide your password']
+	},
 
+	passwordConfirm: {
+		type: String,
+		required: [true, 'You must confirm your password']
 	},
     
 	email: { 
@@ -46,6 +53,24 @@ const TherapistSchema = new mongoose.Schema({ //Therapist schema that inherits f
 		required: [true, 'Country required']
 	}
 });
+
+// Pre-middleware hook
+TherapistSchema.pre('save', async function(next) {
+	const currentTherapist = this;
+
+	if(!currentTherapist.isModified('password')) {
+		return next();
+	}
+
+	this.password = await bcrypt.hash(HASH_BYTES)
+	this.passwordConfirm = undefined;
+
+	return next(); // Return the next middleware
+});
+
+TherapistSchema.methods.comparePasswords = async function(candidatePassword, userPassword) {
+	return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const Therapists = mongoose.model('Therapist', TherapistSchema);
 module.exports = Therapists; // Export the module
